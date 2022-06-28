ff0769812aaadb57fdc6fa460414e317fa4c1a8f
Ali Hallaji <ali.hallaji1@gmail.com>
2022-06-28 19:39:13 +0430

    update

--- iso-builder.sh
+++ iso-builder.sh
@@ -33,7 +33,7 @@ sudo cp --verbose -p app-root/bin-scripts/unconfigured.sh $WORKING_DIRECTORY/dib
 sudo chmod 755 $WORKING_DIRECTORY/diba-bc-installer/usr/sbin/unconfigured.sh
 sudo chmod 755 $WORKING_DIRECTORY/diba-bc-installer/usr/bin/proxinstall
 sudo rsync -avrP --no-owner --no-group --no-perms app-root/diba-installer/ $WORKING_DIRECTORY/diba-bc-installer/var/lib/proxmox-installer/
-sudo rsync -a --no-owner --no-group --remove-source-files $WORKING_DIRECTORY/diba-bc-installer/var/lib/proxmox-installer/ $WORKING_DIRECTORY/diba-bc-installer/var/lib/diba-bc-installer/
+sudo rsync -a --no-owner --no-group --remove-source-files $WORKING_DIRECTORY/diba-bc-installer/var/lib/proxmox-installer/ $WORKING_DIRECTORY/diba-bc-installer/var/lib/diba-installer/
 
 # Making initrd.img
 echo "Making initrd.img..."
@@ -45,7 +45,7 @@ sudo cpio -i -d < $ISO_SOURCE/boot/initrd.unc
 cd $INSTALLER_PATH
 sudo rsync -avrP --no-owner --no-group --no-perms app-root/initrd-files/ $ISO_SOURCE/boot/tmp/
 sudo cp --verbose -p app-root/bin-scripts/unconfigured.sh $ISO_SOURCE/boot/tmp/sbin/unconfigured.sh
-# sudo rsync -a --no-owner --no-group --remove-source-files $ISO_SOURCE/boot/tmp/.pve-cd-id.txt $ISO_SOURCE/boot/tmp/.diba-bc-cd-id.txt
+# sudo rsync -a --no-owner --no-group --remove-source-files $ISO_SOURCE/boot/tmp/.pbs-cd-id.txt $ISO_SOURCE/boot/tmp/.diba-bc-cd-id.txt
 sudo chmod 755 $ISO_SOURCE/boot/tmp/sbin/unconfigured.sh
 cd $ISO_SOURCE/boot/tmp
 find . | cpio -H newc -o | zstd -19 > $ISO_SOURCE/boot/initrd.img
@@ -57,7 +57,7 @@ echo "removed directory 'tmp'"
 # Preparing deb packages and versions and Making them
 echo "Preparing deb packages and versions and Making them... ."
 cd $ISO_SOURCE/proxmox/packages
-# MANAGER_NAME=$(find . -name "*pve-manager*" -print)
+# MANAGER_NAME=$(find . -name "*pbs-manager*" -print)
 
 # Making widget toolkit
 # echo "Making Diba VC Making widget toolkit as deb package file (.deb)"
@@ -81,7 +81,7 @@ for f in *proxmox*; do mv -v "$f" "${f/proxmox/diba-bc}"; done;
 echo "Making Squash from installer..."
 cd $WORKING_DIRECTORY
 sudo mksquashfs diba-bc-installer/ diba-bc-installer.squashfs
-sudo rm -rvf $ISO_SOURCE/pve-installer.squashfs
+sudo rm -rvf $ISO_SOURCE/pbs-installer.squashfs
 
 # Copy for making iso
 cd $INSTALLER_PATH
@@ -90,12 +90,18 @@ sudo rsync -avrP --no-owner --no-group --no-perms app-root/iso-root-files/ $ISO_
 sudo cp --verbose -p $WORKING_DIRECTORY/diba-bc-installer.squashfs $ISO_SOURCE/
 sudo chmod 644 $ISO_SOURCE/diba-bc-installer.squashfs
 sudo chown root:root $ISO_SOURCE/diba-bc-installer.squashfs
-# sudo rsync -a --no-owner --no-group --remove-source-files $ISO_SOURCE/.pve-cd-id.txt $ISO_SOURCE/.diba-bc-cd-id.txt
+# sudo rsync -a --no-owner --no-group --remove-source-files $ISO_SOURCE/.pbs-cd-id.txt $ISO_SOURCE/.diba-bc-cd-id.txt
+
+# make squash base
+echo "Making Squash base file from installer..."
+sudo mv --verbose $ISO_SOURCE/pbs-base.squashfs $ISO_SOURCE/diba-bc-base.squashfs
+sudo chmod 644 $ISO_SOURCE/diba-bc-base.squashfs
+sudo chown root:root $ISO_SOURCE/diba-bc-base.squashfs
 
 # Final steps
 cd $WORKING_DIRECTORY
 sudo rm -rvf diba-bc-installer.squashfs
-sudo rm -rvf pve-base.squashfs
+sudo rm -rvf pbs-base.squashfs
 sudo rm -rf diba-bc-installer
 sudo rm -rf diba-bc-base
 echo "removed directory 'diba-bc-installer' and 'diba-bc-base'"
@@ -105,7 +111,7 @@ echo "Making ISO starting..."
 cd $ISO_SOURCE
 sudo cp --verbose -p $ROOT_DIR/Packages proxmox/packages/
 sudo chmod 444 proxmox/packages/Packages
-sudo mv proxmox diba-vc
+sudo mv proxmox diba-bc
 sudo xorriso -as mkisofs -o ../"$PRODUCT_NAME".iso -r -V "$PRODUCT_NAME" --grub2-mbr ../dibabc.mbr --protective-msdos-label -efi-boot-part --efi-boot-image  -c '/boot/boot.cat' -b '/boot/grub/i386-pc/eltorito.img' -no-emul-boot -boot-load-size 4 -boot-info-table --grub2-boot-info -eltorito-alt-boot -e '/efi.img' -no-emul-boot .
 cd $WORKING_DIRECTORY
 sudo rm -rf $ISO_SOURCE
